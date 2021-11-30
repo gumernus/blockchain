d3de822e08d4efc472ad0d899c32d4472a6e193f
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-30 16:10:59 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-//TODO: pro blok/block udělat interface
 //TODO: dát chain jako samostatnou class
 //TODO: rozdělit do více souborů
 //TODO: první blok chainu se vždy updatuje podle toho jaký je poslední blok (last "data" = first "prev")
-//TODO: přidat types k věcem které typ ještě nemají
 //TODO: uspořádat syntax a lépe rozdělit funkce
 //TODO: hash funkce přímo do objektu plus salt 
 //TODO: hashovat pořadí bloku a timestamp
@@ -11,32 +9,34 @@
 import randomstring from "randomstring"
 import Block from "./block"
 
-let chain = []
+let chain: Array<Block> = []
 
-if (chain.length === 0){
+if (chain.length === 0) {
     let prev = {
-        data: false, 
-        timestamp: false, 
-        order: false}
-        
-    let blok:any = new Block("block-one", prev, Date.now(), 0)
+        data: false,
+        timestamp: false,
+        order: false
+    }
+
+    let blok: Block = new Block("block-one", prev, Date.now(), 0)
     chain.push(blok)
     console.log(blok)
 }
-if (chain.length > 0){
-    for (let i : number = 1; i<=5;i++){
-        let name:string = randomstring.generate({
+if (chain.length > 0) {
+    for (let i: number = 1; i <= 5; i++) {
+        let name: string = randomstring.generate({
             length: 12,
             charset: 'alphabetic'
-          });
+        });
         let prev = {
-            data: chain[i-1].data, 
-            timestamp: chain[i-1].timestamp, 
-            order: chain[i-1].order}
+            data: chain[i - 1].data,
+            timestamp: chain[i - 1].timestamp,
+            order: chain[i - 1].order
+        }
 
-        let blok:any = new Block(name, prev, Date.now(), i)
+        let blok: Block = new Block(name, prev, Date.now(), i)
         chain.push(blok)
-       console.log(blok)
+        console.log(blok)
     }
 }
 
