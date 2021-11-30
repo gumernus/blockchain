8b6e7ba0961f10385a3c34305479b2c9cba74726
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-30 18:16:24 +0100

    Update transfer.ts

--- src/transfer.ts
+++ src/transfer.ts
@@ -1,1 +1,9 @@
+import Block from "./block"
 
+export default function transferBlock(block: Block, chain: Array<Block>, name: string) {
+    let order: Block["order"] = block.order
+    console.log(block)
+    chain[order].data = name
+    chain[order + 1].prev = { data: name, order: chain[order].order, timestamp: chain[order].timestamp }
+
+}
