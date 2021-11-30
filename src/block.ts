19a0eeb4322106e23593601857c7623f0e670b73
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-30 17:04:57 +0100

    Update block.ts

--- src/block.ts
+++ src/block.ts
@@ -1,12 +1,3 @@
-//FIX: dát "prev" na konec interface a taky ve všech .push
-//FIX: odstranit boolean a v "chain[0]" ho nahradit věcmi který to mají být ale s "0" value
-
-export interface blockInterface {
-    data: string | boolean;
-    prev: Object | boolean;
-    timestamp: number | boolean;
-    order: number | boolean;
-}
 
 export interface prevInterface {
     data: Block["data"] | boolean;
@@ -14,11 +5,12 @@ export interface prevInterface {
     order: Block["order"] | boolean;
 }
 
-export default class Block implements blockInterface {
+export default class Block {
     constructor(
-        public data: string | boolean,
-        public prev: Object | boolean,
-        public timestamp: number | boolean,
-        public order: number | boolean
+        public data: string,
+        public timestamp: number,
+        public order: number,
+        public prev: Object
+
     ) { }
 }
