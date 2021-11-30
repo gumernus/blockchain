5463414e7e9b4562549077b2d1d1b5aa65fa4297
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-30 15:57:58 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 //TODO: pro blok/block udělat interface
-//TODO: přidat timestamp do bloku
 //TODO: dát chain jako samostatnou class
 //TODO: rozdělit do více souborů
-//TODO: dát na github (veřejně)
 //TODO: první blok chainu se vždy updatuje podle toho jaký je poslední blok (last "data" = first "prev")
 //TODO: přidat types k věcem které typ ještě nemají
 //TODO: uspořádat syntax a lépe rozdělit funkce
 //TODO: hash funkce přímo do objektu plus salt 
-//TODO: hashované pořadí bloku
+//TODO: hashovat pořadí bloku a timestamp
 //TODO: přidat do block objektu objekt tradů
 
 import randomstring from "randomstring"
@@ -16,29 +14,38 @@ let chain = []
 
 class block {
     constructor(
-        public data: string,
-        public prev: boolean | Object
-    ){
-        this.data = data
-        this.prev = prev
-    }
+        public data: string | boolean,
+        public prev: Object | boolean,
+        public timestamp: number | boolean,
+        public order: number | boolean
+    ){}
 }
 
 if (chain.length === 0){
-    let blok:any = new block("lmao 1. blok", false)
+    let prev = {
+        data: false, 
+        timestamp: false, 
+        order: false}
+        
+    let blok:any = new block("block-one", prev, Date.now(), 0)
     chain.push(blok)
-    //console.log(blok)
+    console.log(blok)
 }
 if (chain.length > 0){
-    for (let i : number = 0; i<=100;i++){
+    for (let i : number = 1; i<=5;i++){
         let name:string = randomstring.generate({
             length: 12,
             charset: 'alphabetic'
           });
-        let blok:any = new block(i + " " + name, chain[chain.length-1].data)
+        let prev = {
+            data: chain[i-1].data, 
+            timestamp: chain[i-1].timestamp, 
+            order: chain[i-1].order}
+
+        let blok:any = new block(name, prev, Date.now(), i)
         chain.push(blok)
-       //console.log(blok)
+       console.log(blok)
     }
 }
 
-console.log("CHAIN: \n" + JSON.stringify(chain, null, ' '))
+// console.log("CHAIN: \n" + JSON.stringify(chain, null, ' '))
