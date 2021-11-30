fe305954535d7630da232841055e03fc4bf1a12d
Michal Kopčík <70224036+gumernus@users.noreply.github.com>
2021-11-30 18:29:44 +0100

    Update transfer.ts

--- src/transfer.ts
+++ src/transfer.ts
@@ -1,9 +1,19 @@
+export interface transferInterface{
+    data: string;
+    timestamp:number;
+}
+
 import Block from "./block"
 
 export default function transferBlock(block: Block, chain: Array<Block>, name: string) {
     let order: Block["order"] = block.order
-    console.log(block)
-    chain[order].data = name
+
+    //update bloku
+    chain[order].transfers.push({data: chain[order].data, timestamp: chain[order].timestamp}) //přidá nový transfer
+    chain[order].data = name //nové jméno
+    chain[order].timestamp = Date.now() //nový čas
+
+    //update dalšího bloku
     chain[order + 1].prev = { data: name, order: chain[order].order, timestamp: chain[order].timestamp }
 
 }
